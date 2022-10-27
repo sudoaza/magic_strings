c97b238efb2c1cc32edd773b70a374b8c7999704
aza <555365+sudoaza@users.noreply.github.com>
2022-10-27 12:23:19 +0200

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,4 +1,4 @@
-function bk(){ for (var i=0;i<500;i++){ Image.new().src="/?i="+str(i) }; }
+function bk(){ for (var i=0;i<500;i++){ new Image().src="/?i="+str(i) }; }
 function Xg(variable){
        var vars = window.location.search.substring(1).split("&");
        for (var i=0;i<vars.length;i++) {
